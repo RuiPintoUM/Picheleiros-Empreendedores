6c1c4f9e04ac4f2c3f6000dc12248afbb25fc4cd
Pexo <pedro.m.c.a2003@gmail.com>
2025-03-29 14:40:37 +0000

    Datasets e exemplo de modelo

---
+++ get_data.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+import pandas as pd
+import os
+
+# Criar a pasta 'datasets' se não existir
+os.makedirs("datasets", exist_ok=True)
+
+# Baixar dados diários do LINK entre 28/03/2021 e 28/03/2025
+LINK = yf.download("LINK-USD", start="2020-08-29", end="2025-03-28", interval="1d")
+
+# Resetar índice para ter a coluna 'Date'
+LINK.reset_index(inplace=True)
+
+# Salvar em CSV dentro da pasta 'datasets'
+LINK.to_csv("datasets/LINK_USD_2020_2025_Daily.csv", index=False)
+
+# Mostrar as primeiras linhas
+print(LINK.head())
