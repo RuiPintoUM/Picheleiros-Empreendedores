74aa9deb19bb7c802e059e0965f03ac967aaf786
Rui Pinto <rfernandespinto@gmail.com>
2025-03-30 05:14:29 +0100

    Testes com modelo diferente e ceansd e ME

--- merge_ME.py
+++ merge_ME.py
@@ -2,15 +2,27 @@ import pandas as pd
 
 # Carregar datasets
 basquet = pd.read_csv("basket_with_rsi_and_ma14.csv")
+
 major_events = pd.read_csv("datasets/crypto_major_events.csv")
 
 # Garantir que as datas estão em datetime
 basquet['Data'] = pd.to_datetime(basquet['Date'], format='%Y-%m-%d')
-major_events['Data'] = pd.to_datetime(major_events['Data'], format='%Y-%m-%d')
+major_events['Date'] = pd.to_datetime(major_events['Data'], format='%Y-%m-%d')
 
 # Adicionar coluna 'ME' ao dataset basquet (1 se for um evento major, 0 caso contrário)
 basquet['ME'] = basquet['Data'].isin(major_events['Data']).astype(int)
 
+if basquet['ME'].sum() > 0:
+    if major_events['Impacto'].equals("Moderado"):
+        basquet['ME_Impact'] = 1
+    elif major_events['Impacto'].equals("Alto"):
+        basquet['ME_Impact'] = 2
+    elif major_events['Impacto'].equals("Muito Alto"):
+        basquet['ME_Impact'] = 3
+else:
+    basquet['ME_Impact'] = 0
+        
+        
 
 
 # Salvar novo dataset atualizado, se necessário
